a13a0fc45ea638f4bca73623d8bab87dbdc75706
cm <1222114308@qq.com>
2019-10-20 22:41:54 +0800

    增加了数据库操作，一些用例跑完才会在数据库中产生数据的，要通过查数据库来替换

--- tools/do_mysql.py
+++ tools/do_mysql.py
@@ -1,24 +1,29 @@
 import mysql.connector
-config={
-    'host':'127.0.0.1',
-    'user':'root',
-    'password':'123456',
-    'port':3306,
-    'database':'bookmall',
-}
-#创建一个数据库连接
-cnn = mysql.connector.connect(**config)
-#游标cursor
-cursor=cnn.cursor()
-#写sql语句
-query_sql='select * from books where id=1'
-#执行语句
-cursor.execute(query_sql)
-#获取结果 打印结果
-res=cursor.fetchone() #针对一条数据 返回的是元组
-#cursor.fetchall() 针对多条数据 返回的是列表
-print(res)
-#关闭游标
-cursor.close()
-#关闭连接
-cnn.close()
+from tools.read_config import ReadConfig
+from tools.project_path import *
+class DoMySQL:
+    def do_mysql(self,query,state='all'):    #query-->查询语句
+        config=eval(ReadConfig.read_config(test_config_path,'DB','config'))
+        #创建一个数据库连接
+        cnn = mysql.connector.connect(**config)
+        #游标cursor
+        cursor=cnn.cursor()
+        #执行语句
+        cursor.execute(query)
+        #获取结果 打印结果
+        if state==1:
+            res=cursor.fetchone() #针对一条数据 返回的是元组
+        else:
+            res=cursor.fetchall() #针对多条数据 返回的是列表
+        #关闭游标
+        cursor.close()
+        #关闭连接
+        cnn.close()
+        return res
+if __name__ == '__main__':
+    do=DoMySQL()
+    #res=do.do_mysql('select * from books where id=1')
+    #print(res[0][0])
+    res=do.do_mysql('select * from books where id=1',1)
+    print(res[0])
+
